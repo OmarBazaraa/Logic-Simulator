59912794c1c494cd8267569801e7ce122bade75b
omarosamasobeih96 <omarosamasobeih@yahoo.com>
2016-05-18 03:07:24 +0200

    Truth Table

--- Actions/TruthTable.cpp
+++ Actions/TruthTable.cpp
@@ -5,11 +5,13 @@
 TruthTable::TruthTable(ApplicationManager* pAppMan) : Action(pAppMan) {
 	switchesCount = 0;
 	ledsCount = 0;
-	
+	write.open("TruthTable.txt");
+	write.clear();
 	// Create and initialize the drawing window
 	pWind = CreateWind(UI.Width, UI.Height, UI.StartX, UI.StartY);
 	ChangeTitle("Truth Table");
 	ClearDrawingArea();
+	canDraw = 1;
 }
 
 /* Creates a new Window object */
@@ -29,69 +31,42 @@ void TruthTable::ClearDrawingArea() const {
 	pWind->SetPen(BACKGROUND_BLUE);
 	pWind->SetBrush(BACKGROUND_BLUE);
 	pWind->DrawRectangle(0, 0, UI.Width, UI.Height);
-	/*
-	//
-	// JUST FOR TESTING
-	//
-	pWind->SetPen(WHITE, 1);
 
-	int startX = 0;
-	int endX = UI.HorPinsCount * UI.PinOffset;
-	int startY = UI.ToolBarHeight + UI.GateBarHeight;
-	int endY = startY + UI.VerPinsCount * UI.PinOffset;
-
-	// Vertical lines
-	for (int x = startX; x <= endX; x += UI.PinOffset) {
-		pWind->DrawLine(x, startY, x, endY);
-	}
-
-	// Horizontal lines
-	for (int y = startY; y <= endY; y += UI.PinOffset) {
-		pWind->DrawLine(startX, y, endX, y);
-	}
-
-	startX += UI.PinMargin;
-	endX -= UI.PinMargin;
-	startY += UI.PinMargin;
-	endY -= UI.PinMargin;
-
-	// Pins
-	for (int x = startX; x <= endX; x += UI.PinOffset) {
-		for (int y = startY; y <= endY; y += UI.PinOffset) {
-			pWind->DrawPixel(x, y);
-		}
-	}*/
 }
 
 /*Draws truth table*/
 void TruthTable::DrawTruthTable() const {
 	// Vertical lines
 	pWind->SetPen(WHITE);
-	for (int x = 0; x <= (columns+1)*100; x += 100) {
-		pWind->DrawLine(x, 100, x, (2+rows)*100);
+	for (int x = 0; x <= (columns + 1) * 100; x += 100) {
+		pWind->DrawLine(x, 20, x, (2 + rows) * 20);
 	}
 
 	// Horizontal lines
-	for (int y = 0; y <= (2+rows)*100; y += 100) {
-		pWind->DrawLine(100, y, (1+columns)*100, y);
+	for (int y = 0; y <= (2 + rows) * 20; y += 20) {
+		pWind->DrawLine(100, y, (1 + columns) * 100, y);
 	}
 }
 
 bool TruthTable::Execute() {
 
-	Component ** list=mAppManager->GetComponentList();
+	Component ** list = mAppManager->GetComponentList();
 	int count = mAppManager->GetComponentsCount();
 	leds = new LED*[count];
 	switches = new Switch*[count];
 	for (int i = 0; i < count; i++) {
-		if (dynamic_cast<LED*>(list[i]))
+		if (dynamic_cast<LED*>(list[i]) && !list[i]->IsDeleted())
 			leds[ledsCount++] = (LED*)list[i];
-		else if (dynamic_cast<Switch*>(list[i]))
+		else if (dynamic_cast<Switch*>(list[i]) && !list[i]->IsDeleted())
 			switches[switchesCount++] = (Switch*)list[i];
 	}
 	rows = pow(2, switchesCount);
 	columns = ledsCount + switchesCount;
-	DrawTruthTable();
+	if (switchesCount < 6 && columns < 12) {
+		DrawTruthTable();
+	}
+	else
+		canDraw = 0;
 	DrawHeaders();
 	CreateCompinations("");
 
@@ -100,19 +75,23 @@ bool TruthTable::Execute() {
 
 
 /*Draw headers*/
-void TruthTable::DrawHeaders() const {
+void TruthTable::DrawHeaders() {
 	pWind->SetPen(UI.MsgColor);
 	pWind->SetFont(UI.StatusFontSize, BOLD, BY_NAME, "Arial");
 	string msg = "header";
 	int n = switchesCount;
 	int k = 0;
+
 	for (int i = 110; i <= columns * 100 + 10; i += 100) {
 		if (i / 100 <= n)
-			msg = "sw ID " + to_string(switches[i / 100 - 1]->GetID());
+			msg = "swh ID " + to_string(switches[i / 100 - 1]->GetID());
 		else
 			msg = "led ID " + to_string(leds[k++]->GetID());
-		pWind->DrawString(i, 140, msg);
+		if (canDraw)
+			pWind->DrawString(i, 20, msg);
+		write << msg << "   ";
 	}
+	write << endl;
 }
 
 /*Create Compinations*/
@@ -123,27 +102,39 @@ void TruthTable::CreateCompinations(string k) {
 }
 
 void TruthTable::Test(string k) {
-	int pos = ToInt(k) * 100 + 240;
+	int pos = ToInt(k) * 20 + 40;
 	string h;
 	for (int i = 0; i < switchesCount; i++) {
-		//switches[i]->GetOutptPin().SetStatus((Status)(k[i] - '0'));
+		switches[i]->GetOutputPin()->SetStatus(Status(k[i] - '0'));
 		h = k[i];
-		pWind->DrawString((i + 1) * 100 + 45, pos, h);
+		if (canDraw)pWind->DrawString((i + 1) * 100 + 45, pos, h);
+		write << "   " << h << "       ";
 	}
 	for (int i = 0; i < ledsCount; i++) {
 		h = "";
-		h+=('0'+TestGate(leds[i]));
+		h += ('0' + TestGate(leds[i]));
+		if (h[0] - '0')pWind->SetPen(GREEN);
+		else pWind->SetPen(RED);
+		if (canDraw)pWind->DrawString((i + 1) * 100 + 45 + switchesCount * 100, pos, h);
+		write << "   " << h << "       ";
+		pWind->SetPen(WHITE);
 	}
+	write << endl;
 }
 
 /*Tests the output on a led*/
 int TruthTable::TestGate(Component*c) {
 	if (dynamic_cast<Switch*>(c))return c->GetOutputPinStatus();
-	/*if (dynamic_cast<LED*>(c))return TestGate(((LED*)c)->GetInputPin().GetComponent());
-	if (dynamic_cast<Connection*>(c))
-	{
-		return TestGate(((Connection*)c)->GetSourcePin()->GetComponent());
-	}*/
+	if (dynamic_cast<LED*>(c))return TestGate(((LED*)c)->GetInputPin(0)->GetComponent());
+	if (dynamic_cast<Connection*>(c))return TestGate(((Connection*)c)->GetSourcePin()->GetComponent());
+	else if (dynamic_cast<LogicGate*>(c)) {
+		for (int i = 0;; i++)
+			if (((LogicGate*)c)->GetInputPin(i)->GetComponent())
+				((LogicGate*)c)->SetInputPinStatus(i, Status(TestGate(((LogicGate*)c)->GetInputPin(i)->GetComponent())));
+			else break;
+			((LogicGate*)c)->Operate();
+			return ((LogicGate*)c)->GetOutputPinStatus();
+	}
 }
 
 int TruthTable::ToInt(string k) {
